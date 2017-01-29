7e6c36a76e1690316f256a85ff2dabca488135b2
Sirplentifus <simao.marto@gmail.com>
2017-01-29 15:22:33 +0000

    analyse_collision_danger

--- TCAS_sim.h
+++ TCAS_sim.h
@@ -17,30 +17,36 @@ class TCAS_sim
 {
 public:
     const static int NumOfTargets = 10; //Maximum???
-
+    
     
 private:
     
     AC_sim own_State_sim;
     TCAS_state own_TCAS_State;
-    
     broadcast_socket* socket_ptr;
-    
-    void sim_thread_fn();
     
     std::vector<AC_state> targetStates;
     std::vector<TCAS_state> target_TCAS_States;
     
+    //Used to communicate with the socket
+    void UpdateOwnState();
+    void UpdateTargetStates();
+    
+    //Function to have its own thread, and its thread handler
+    void sim_thread_fn();
     std::thread sim_thread;
+    
+    //Performs the decisions characterstic of a TCAS system
+    void Actual_TCAS();
+    const double min_safe_distance = 1e3; //m
+    bool analyse_collision_danger(const int which_target, double &time_to_approach); //returns weather this aircraft will be or is within min_safe_distance of our aircraft
+    
     
 public:
 
     TCAS_sim(AC_sim new_State_sim, broadcast_socket* new_socket_ptr);
     
-    
-    void UpdateOwnState();
-    void UpdateTargetStates(); //Using const qualifier gives error
-    
 };
- 
+
+double internal_product(double A[3], double B[3]);
 void printState (AC_state state);
