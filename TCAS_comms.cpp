b0a007dab7c8de455143d25b7f39f1ffa0969412
ericloewe <ericloewe@outlook.pt>
2017-02-06 23:56:55 +0000

    Copyright info and files cleanup

--- TCAS_comms.cpp
+++ TCAS_comms.cpp
@@ -1,4 +1,37 @@
-//TCAS_comms.cpp
+/*
+    Group C TCAS Project
+https://github.com/ericloewe/TCAS
+
+Sistemas Aviónicos Integrados 2016/2017
+Instituto Superior Técnico
+
+Copyright 2017 
+
+Simão Marto     75326
+Eric Loewenthal 75848
+João Martins    76964
+
+This software is licensed under the terms of the GNU General Public License version 3.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+TCAS_comms: Definitions related to networking
+
+All actual networking happens here
+
+ */
 
 #include <iostream>
 #include <cstdlib>
@@ -474,4 +507,116 @@ int broadcast_socket::getUpdatedTargetsStatus(
     
     
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ *          Group C TCAS Project
+ *  https://github.com/ericloewe/TCAS
+ *
+ *  Sistemas Aviónicos Integrados 2016/2017
+ *  Instituto Superior Técnico
+ *
+ *  Copyright 2017 
+ *  Simão Marto     75326
+ *  Eric Loewenthal 75848
+ *  João Martins    76964
+ *
+ *  This software is licensed under the terms of the 
+ *  GNU General Public License version 3.
+ *
+ *
+ *  This copyright notice is here to help spot dirty thieves
+ *
+ */   
--- TCAS_sim.cpp
+++ TCAS_sim.cpp
@@ -1,3 +1,36 @@
+/*
+    Group C TCAS Project
+https://github.com/ericloewe/TCAS
+
+Sistemas Aviónicos Integrados 2016/2017
+Instituto Superior Técnico
+
+Copyright 2017 
+
+Simão Marto     75326
+Eric Loewenthal 75848
+João Martins    76964
+
+This software is licensed under the terms of the GNU General Public License version 3.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+TCAS_sim: The actual TCAS work happens here
+
+ */
+
 #include "TCAS_sim.h"
 
 TCAS_sim::TCAS_sim(AC_sim new_State_sim, broadcast_socket* new_socket_ptr){
@@ -281,6 +314,31 @@ double internal_product(double A[3], double B[3]){
     return ret;
 }
 
+
+
+
+/*
+ *          Group C TCAS Project
+ *  https://github.com/ericloewe/TCAS
+ *
+ *  Sistemas Aviónicos Integrados 2016/2017
+ *  Instituto Superior Técnico
+ *
+ *  Copyright 2017 
+ *  Simão Marto     75326
+ *  Eric Loewenthal 75848
+ *  João Martins    76964
+ *
+ *  This software is licensed under the terms of the 
+ *  GNU General Public License version 3.
+ *
+ *
+ *  This copyright notice is here to help spot dirty thieves
+ *
+ */
+
+
+//Debug
 void printState (AC_state state)
 {
     cout << endl;
@@ -309,6 +367,7 @@ void printState (AC_state state)
     cout << "Velocity: " << state.x_spd << "; " << state.y_spd << "; " << state.z_spd << endl;*/
 }
 
+//Simple getter
 AC_sim TCAS_sim::getAC_sim()
 {
     return own_State_sim;
